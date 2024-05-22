4c717b8817b7f2a59ebd94b4c115593b346df7fc
Marlon Lobato <marlonbrenolobatoalves@gmail.com>
2024-05-22 15:40:08 -0300

    Apenas testes

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        String primeiroNome = "Marlon Lobato";
+        String primeiroNome = "Marlon Breno";
         String simbol = "& ";
         String SegundoNome = "Gabriela Rosa";
 
