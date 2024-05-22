9c95e610014e1c267125b2efbd06385d542faf04
Marlon Lobato <marlonbrenolobatoalves@gmail.com>
2024-05-22 12:40:29 -0300

    Criados Código, Aulas de Java

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String primeiroNome = "Marlon Breno";
+        String simbol = "& ";
+        String SegundoNome = "Gabriela Rosa";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, simbol, SegundoNome);
+
+        System.out.println(nomeCompleto);
+        }
+
+        
+    public static String nomeCompleto (String primeiroNome, String SegundoNome, String simbol){
+            return primeiroNome.concat(" ").concat(SegundoNome).concat(simbol);
+            
+        }
+    
+}
